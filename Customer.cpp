40c39dff4982755891869033a91036f28a9e2d62
mrpepci <mrpepci@outlook.es>
2021-03-25 22:12:32 -0600

    starting constructors

---
+++ Customer.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <vector>
+#include "Customer.h"
+
+using namespace std;
+
+Customer::Customer()
+{
+    customerName="";
+    size=50;
+    purchase[size];
+}
+Customer::Customer()
+{
+
+}
