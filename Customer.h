24a66bf184b8896dbb609dd92951f035e0df5c66
mrpepci <mrpepci@outlook.es>
2021-03-25 21:38:35 -0600

    clase de clientes v0.01

---
+++ Customer.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Customer
+{
+private:
+    string customerName;
+    int purchase[50];
+    int size;
+public:
+    Customer();
+    Customer(string,int[],int);
+    string getCustomerName(string);
+    void setCustomerName();
+};
+
